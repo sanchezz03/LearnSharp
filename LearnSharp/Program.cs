6e738c31eecab446b09ebce0b2f2159b26ea3d30
alex.vashenkov2018@gmail.com <alex.vashenkov2018@gmail.com>
2022-04-02 03:26:46 +0300

    modified hello world to first change

--- LearnSharp/Program.cs
+++ LearnSharp/Program.cs
@@ -6,7 +6,7 @@ namespace LearnSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello first change!");
         }
     }
 }
