c2e0d236a4c7bab425d9e4e5c9de78fff1e68023
alex.vashenkov2018@gmail.com <alex.vashenkov2018@gmail.com>
2022-04-02 03:23:22 +0300

    Add project files.

---
+++ LearnSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
